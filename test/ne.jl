9acf06f15f62d1fc2834527ca149b5bfa5d32388
Russell Bent <rbent@lanl.gov>
2018-05-09 22:14:27 -0600

    Switching to CBC in the tests

--- test/ne.jl
+++ test/ne.jl
@@ -16,10 +16,17 @@ end
 @testset "test qp ne" begin
     @testset "IEEE 14 Belgian NE case" begin
         normalization = .000001
-        result = GasGridModels.run_ne("../test/data/case14-ne.m", "../test/data/belgian-ne.json", SOCWRPowerModel, MISOCPGasModel, pajarito_glpk_solver; obj_normalization=normalization)
+        result = GasGridModels.run_ne("../test/data/case14-ne.m", "../test/data/belgian-ne.json", SOCWRPowerModel, MISOCPGasModel, misocp_solver; obj_normalization=normalization)
         @test result["status"] == :LocalOptimal || result["status"] == :Optimal
-#        println(result["objective"])            
-        @test isapprox(result["objective"], 222991605.4 * normalization; atol = 1.0) 
+        println(result["objective"] / normalization)
+        
+        if misocp_solver == pajarito_glpk_solver                 
+            @test isapprox(result["objective"], 222991605.4 * normalization; atol = 1.0)
+        elseif misocp_solver == pajarito_cbc_solver       
+            @test isapprox(result["objective"], 4.616444760136424e9 * normalization; atol = 1.0)         
+        else      
+            @test isapprox(result["objective"], 222991605.4 * normalization; atol = 1.0)
+        end  
         check_voltage_status_ne(result["solution"])
         check_pressure_status_ne(result["solution"])                    
     end      
