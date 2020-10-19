06350f6beddac7a89b3dcd8544f1a35466ccb00a
Russell Bent <rbent@lanl.gov>
2020-10-19 10:22:03 -0600

    Fixed escape characters

--- src/core/variable.jl
+++ src/core/variable.jl
@@ -8,7 +8,7 @@ function getstart(set, item_key, value_key, default=0.0)
     return get(get(set, item_key, Dict()), value_key, default)
 end
 
-"Function for creating variables associated with zonal demand: ``\psi``"
+"Function for creating variables associated with zonal demand: ``\\psi`` "
 function variable_zone_demand(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
     junctions = filter(x -> haskey(x.second, "price_zone") && x.second["price_zone"] != 0, _GM.ref(gm, n, :junction))
     fl_max = Dict{Int,Float64}(i => 0.0 for i in _GM.ids(gm, n, :price_zone))
@@ -28,7 +28,7 @@ function variable_zone_demand(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
         start=getstart(_GM.ref(gm, n, :price_zone), i, "zone_fl_start", 0.0))
 end
 
-"Function for creating variables associated with zonal demand price: ``\gamma``"
+"Function for creating variables associated with zonal demand price: ``\\gamma`` "
 function variable_zone_demand_price(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
     gm.var[:nw][n][:zone_cost] = JuMP.@variable(gm.model,
         [i in keys(gm.ref[:nw][n][:price_zone])],
@@ -36,7 +36,7 @@ function variable_zone_demand_price(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
         start=getstart(_GM.ref(gm,n,:price_zone), i, "zone_cost_start", 0.0))
 end
 
-"Function for creatinf variables associated with zonal pressure: ``\rho``"
+"Function for creatinf variables associated with zonal pressure: ``\\rho`` "
 function variable_zone_pressure(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
     junctions = filter(x -> haskey(x.second, "price_zone") && x.second["price_zone"] != 0, _GM.ref(gm, n, :junction))
     p_min, p_max = Dict{Int,Any}(), Dict{Int,Any}()
@@ -54,7 +54,7 @@ function variable_zone_pressure(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
         start=getstart(_GM.ref(gm, n, :price_zone), i, "zone_p_start", 0.0))
 end
 
-"Function for creating variables associated with zonal pressure price: ``\omega``"
+"Function for creating variables associated with zonal pressure price: ``\\omega`` "
 function variable_pressure_price(gm::_GM.AbstractGasModel, n::Int=gm.cnw)
     junctions = filter(x -> haskey(x.second, "price_zone") && x.second["price_zone"] != 0, _GM.ref(gm, n, :junction))
     p_min, p_max = Dict{Int,Any}(), Dict{Int,Any}()
