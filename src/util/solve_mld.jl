f163fccbac4fb13507d1a61000c02bc51f7f5467
Byron Tasseff <btasseff@lanl.gov>
2020-12-09 14:21:41 -0700

    Add convenience function and checks while solving MLD.

--- src/util/solve_mld.jl
+++ src/util/solve_mld.jl
@@ -4,23 +4,27 @@ function solve_mld_gas_prioritized(data::Dict{String, Any}, model_type::Type, op
     gpm = instantiate_model(data, model_type, build_mld; kwargs...)    
     result_1 = _IM.optimize_model!(gpm, optimizer = optimizer)
 
-    # Set up the MLD problem with power prioritized.
-    data["ng_load_priority"], data["ep_load_priority"] = 0.0, 1.0
-    gpm = instantiate_model(data, model_type, build_mld; kwargs...) 
-    gas_obj_expr = objective_max_gas_load(gpm) # Get the gas objective.
-    c = JuMP.@constraint(gpm.model, gas_obj_expr >= result_1["objective"])
-    power_obj_expr = objective_max_power_load(gpm) # Set the power objective.
+    if result_1["termination_status"] in [TIME_LIMIT, INFEASIBLE]
+        return result_1
+    else
+        # Set up the MLD problem with power prioritized.
+        data["ng_load_priority"], data["ep_load_priority"] = 0.0, 1.0
+        gpm = instantiate_model(data, model_type, build_mld; kwargs...) 
+        gas_obj_expr = objective_max_gas_load(gpm) # Get the gas objective.
+        c = JuMP.@constraint(gpm.model, gas_obj_expr >= result_1["objective"])
+        power_obj_expr = objective_max_power_load(gpm) # Set the power objective.
 
-    # Solve the final MLD problem.
-    sol_proc = [_GM.sol_psqr_to_p!, _PM.sol_data_model!] 
-    sol_proc = transform_solution_processors(gpm, sol_proc)
-    result_2 = _IM.optimize_model!(gpm, optimizer = optimizer, solution_processors = sol_proc)
+        # Solve the final MLD problem.
+        sol_proc = [_GM.sol_psqr_to_p!, _PM.sol_data_model!] 
+        sol_proc = transform_solution_processors(gpm, sol_proc)
+        result_2 = _IM.optimize_model!(gpm, optimizer = optimizer, solution_processors = sol_proc)
 
-    # Include both solve times in the returned solution.
-    result_2["solve_time"] += result_1["solve_time"]
+        # Include both solve times in the returned solution.
+        result_2["solve_time"] += result_1["solve_time"]
 
-    # Return the result dictionary.
-    return result_2
+        # Return the result dictionary.
+        return result_2
+    end
 end
 
 function solve_mld_power_prioritized(data::Dict{String, Any}, model_type::Type, optimizer; kwargs...)
@@ -29,21 +33,50 @@ function solve_mld_power_prioritized(data::Dict{String, Any}, model_type::Type, 
     gpm = instantiate_model(data, model_type, build_mld; kwargs...)    
     result_1 = _IM.optimize_model!(gpm, optimizer = optimizer)
 
-    # Set up the MLD problem with gas prioritized.
-    data["ng_load_priority"], data["ep_load_priority"] = 1.0, 0.0
-    gpm = instantiate_model(data, model_type, build_mld; kwargs...)
-    power_obj_expr = objective_max_power_load(gpm) # Get the power objective.
-    c = JuMP.@constraint(gpm.model, power_obj_expr >= result_1["objective"])
-    gas_obj_expr = objective_max_gas_load(gpm) # Get the gas objective.
+    if result_1["termination_status"] in [TIME_LIMIT, INFEASIBLE]
+        return result_1
+    else
+        # Set up the MLD problem with gas prioritized.
+        data["ng_load_priority"], data["ep_load_priority"] = 1.0, 0.0
+        gpm = instantiate_model(data, model_type, build_mld; kwargs...)
+        power_obj_expr = objective_max_power_load(gpm) # Get the power objective.
+        c = JuMP.@constraint(gpm.model, power_obj_expr >= result_1["objective"])
+        gas_obj_expr = objective_max_gas_load(gpm) # Get the gas objective.
 
-    # Solve the final MLD problem.
-    sol_proc = [_GM.sol_psqr_to_p!, _PM.sol_data_model!] 
-    sol_proc = transform_solution_processors(gpm, sol_proc)
-    result_2 = _IM.optimize_model!(gpm, optimizer = optimizer, solution_processors = sol_proc)
+        # Solve the final MLD problem.
+        sol_proc = [_GM.sol_psqr_to_p!, _PM.sol_data_model!] 
+        sol_proc = transform_solution_processors(gpm, sol_proc)
+        result_2 = _IM.optimize_model!(gpm, optimizer = optimizer, solution_processors = sol_proc)
 
-    # Include both solve times in the returned solution.
-    result_2["solve_time"] += result_1["solve_time"]
+        # Include both solve times in the returned solution.
+        result_2["solve_time"] += result_1["solve_time"]
 
-    # Return the result dictionary.
-    return result_2
+        # Return the result dictionary.
+        return result_2
+    end
+end
+
+function solve_mld(data::Dict{String, Any}, model_type::Type, optimizer, alpha::Float64; kwargs...)
+    data["ng_load_priority"], data["ep_load_priority"] = alpha, 1.0 - alpha
+    
+    if alpha <= 0.0
+        result = solve_mld_gas_prioritized(data, model_type, optimizer)
+    elseif alpha >= 1.0
+        result = solve_mld_power_prioritized(data, model_type, optimizer)
+    else
+        result = run_mld(data, model_type, optimizer)
+    end
+
+    if result["primal_status"] == FEASIBLE_POINT
+        gas_load_served = sum([delivery["fd"] for (i, delivery) in result["solution"]["it"]["ng"]["delivery"]])
+        result["gas_load_served"] = gas_load_served
+
+        active_power_served = sum([abs(load["pd"]) for (i, load) in result["solution"]["it"]["ep"]["load"]])
+        result["active_power_served"] = active_power_served
+
+        reactive_power_served = sum([abs(load["qd"]) for (i, load) in result["solution"]["it"]["ep"]["load"]])
+        result["reactive_power_served"] = reactive_power_served
+    end
+
+    return result
 end
