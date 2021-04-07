8e48173f3ed0c75d19e24c4c3dd0dbefeed7ff4e
Byron Tasseff <btasseff@lanl.gov>
2021-04-07 13:20:47 -0600

    Update constraint template and quickstart documentation.

--- src/core/constraint_template.jl
+++ src/core/constraint_template.jl
@@ -1,22 +1,30 @@
-#
 # Constraint Template Definitions
 #
-# Constraint templates help simplify data wrangling across multiple
-# formulations by providing an abstraction layer between the network data
-# and network constraint definitions. The constraint template's job is to
-# extract the required parameters from a given network data structure and
-# pass the data as named arguments to the Gas Flow or Power Flow formulations.
+# Constraint templates help simplify data wrangling across multiple formulations by
+# providing an abstraction layer between network data and network constraint definitions.
+# Each constraint template's job is to extract the required parameters from a given network
+# data structure and pass the data as named arguments to formulation-specific constraints.
 #
-# Constraint templates should always be defined over "AbstractGasModel" and
-# "AbstractPowerModel" and should never refer to model variables.
+# Constraint templates should always be defined over "AbstractGasPowerModel" and should
+# never refer to model variables.
 
 
-"Constraint for coupling the production of power at natural gas generators with the gas consumption required to produce this power.
-The full non convex constraint is stated as ``fl = e * \\rho \\frac{h_2 * pg^2 + h_1 * pg + h_0}{3600}``
-where ``h`` is a quadratic function used to convert MW (``pg``) into Joules consumed per second (J/s). ``h`` is in units of (J/MW^2, J/MW, J).
-This is then converted to mass flow, ``fl``, (kg/s) of gas consumed to produce this energy.
-Here, ``e`` is an energy factor (m^3/J) and ``\\rho`` is standard density (kg/m^3). This constraint can be relaxed to
-a convex quadractic of the form ``fl \\ge e * \\rho (h_2 * pg^2 + h_1 * pg + h_0)``"
+"Constraint for coupling the production of power at natural gas generators with the gas
+consumption required to produce this power. The full nonconvex constraint is stated as
+
+```math
+fl = e * \\rho (h_2 * pg^2 + h_1 * pg + h_0),
+```
+
+where ``h`` is a quadratic function used to convert MW (``pg``) into Joules consumed per
+second (J/s). ``h`` is in units of (J/MW^2, J/MW, J). This is then converted to mass
+flow, ``fl``, (kg/s) of gas consumed to produce this energy. Here, ``e`` is an energy
+factor (m^3/J) and ``\\rho`` is standard density (kg/m^3). This constraint can be relaxed
+to a convex quadratic of the form
+
+```math
+fl \\ge e * \\rho (h_2 * pg^2 + h_1 * pg + h_0).
+```"
 function constraint_heat_rate(gpm::AbstractGasPowerModel, delivery_id::Int; nw::Int = nw_id_default)
     delivery_gens = _IM.ref(gpm, :dep, nw, :delivery_gen)
     dep_ids = findall(x -> x["delivery"]["id"] == delivery_id, delivery_gens)
@@ -50,12 +58,24 @@ function constraint_heat_rate(gpm::AbstractGasPowerModel, delivery_id::Int; nw::
 end
 
 
-"Constraint for coupling the production of power at natural gas generators with the gas consumption required to produce this power.
-The full non convex constraint is stated as ``fl = e * \\rho \\frac{h_2 * pg^2 + h_1 * pg + h_0}{3600}``
-where ``h`` is a quadratic function used to convert MW (``pg``) into Joules consumed per second (J/s). ``h`` is in units of (J/MW^2, J/MW, J).
-This is then converted to mass flow, ``fl``, (kg/s) of gas consumed to produce this energy.
-Here, ``e`` is an energy factor (m^3/J) and ``\\rho`` is standard density (kg/m^3). This constraint can be relaxed to
-a convex quadractic of the form ``fl \\ge e * \\rho (h_2 * pg^2 + h_1 * pg + h_0)``"
+"Constraint for coupling the production of power at dispatchable natural gas generators
+with the gas consumption required to produce this power. The full nonconvex constraint is
+stated as
+
+```math
+fl = e * \\rho (h_2 * pg^2 + h_1 * pg + h_0 * z),
+```
+
+where ``h`` is a quadratic function used to convert MW (``pg``) into Joules consumed per
+second (J/s). ``h`` is in units of (J/MW^2, J/MW, J). This is then converted to mass
+flow, ``fl``, (kg/s) of gas consumed to produce this energy. Here, ``e`` is an energy
+factor (m^3/J) and ``\\rho`` is standard density (kg/m^3). ``z`` is a discrete variable
+indicating the status of the generator. This constraint can be relaxed to a convex
+quadratic of the form
+
+```math
+fl \\ge e * \\rho (h_2 * pg^2 + h_1 * pg + h_0 * z).
+```"
 function constraint_heat_rate_on_off(gpm::AbstractGasPowerModel, delivery_id::Int; nw::Int = nw_id_default)
     delivery_gens = _IM.ref(gpm, :dep, nw, :delivery_gen)
     dep_ids = findall(x -> x["delivery"]["id"] == delivery_id, delivery_gens)
@@ -89,9 +109,15 @@ function constraint_heat_rate_on_off(gpm::AbstractGasPowerModel, delivery_id::In
 end
 
 
-"Auxiliary constraint that computes the total consumed gas in a zones. This constraint takes the form of
-``fl_{z} = \\sum_{k \\in z} fl_k `` where ``fl_{z}`` is the total consumed gas in zone ``z`` and ``fl_k``
-is gas consumed at delivery point ``k`` in the zone. "
+"Auxiliary constraint that computes the total consumed gas in a zone. This constraint
+takes the form of
+
+```math
+fl_{z} = \\sum_{k \\in z} fl_k,
+```
+
+where ``fl_{z}`` is the total consumed gas in zone ``z`` and ``fl_k`` is gas consumed at
+delivery ``k`` in the zone."
 function constraint_zone_demand(gpm::AbstractGasPowerModel, i::Int; nw::Int=nw_id_default)
     if !haskey(_IM.con(gpm, _GM.gm_it_sym, nw), :zone_demand)
         _IM.con(gpm, _GM.gm_it_sym, nw)[:zone_demand] = Dict{Int, JuMP.ConstraintRef}()
@@ -105,12 +131,24 @@ function constraint_zone_demand(gpm::AbstractGasPowerModel, i::Int; nw::Int=nw_i
 end
 
 
-"Constraint that is used to compute cost for gas in a zone.  Since the cost of gas typically appears in the objective function or is bounded,
- these constraints do not compute the price directly, rather they place a lower bound on the price of gas.  There are two constraints stated here.
- The first constraint is ``cost_{z} \\ge q_z[1] * (fl_z * \\frac{1.0}{\\rho})^2 + q_z[2] * fl_z * \\frac{1.0}{\\rho} + q_z[3].
- The second constraint is ``m_z * fl_z * \\frac{1.0}{\\rho} ``
- where ``cost_{z}`` is the daily (24 hour) cost of gas in zone ``z``. ``q`` is the quadractic cost of gas as function of
- gas consumed in the gas, ``fl_z.``  ``\\rho`` is standard density. ``m`` is the minmum cost of gas in terms kg/s."
+"Constraint that is used to compute cost for gas in a zone. Since the cost of gas
+typically appears in the objective function or is bounded, these constraints do not
+compute the price directly. Rather, they place a lower bound on the price of gas. There
+are two constraints stated here. The first constraint is
+
+```math
+cost_{z} \\ge q_z[1] * (fl_z * \\frac{1.0}{\\rho})^2 + q_z[2] * fl_z * \\frac{1.0}{\\rho} + q_z[3].
+```
+
+The second constraint is
+
+```math
+m_z * fl_z * \\frac{1.0}{\\rho},
+```
+
+where ``cost_{z}`` is the daily (24-hour) cost of gas in zone ``z``. ``q`` is the
+quadratic cost of gas as function of gas consumed in the zone, ``fl_z.`` ``\\rho`` is
+standard density. ``m`` is the minimum cost of gas in terms kg/s."
 function constraint_zone_demand_price(gpm::AbstractGasPowerModel, i::Int; nw::Int = nw_id_default)
     if !haskey(_IM.con(gpm, _GM.gm_it_sym, nw), :zone_demand_price)
         _IM.con(gpm, _GM.gm_it_sym, nw)[:zone_demand_price] = Dict{Int, Array{JuMP.ConstraintRef}}()
@@ -123,12 +161,17 @@ function constraint_zone_demand_price(gpm::AbstractGasPowerModel, i::Int; nw::In
 end
 
 
-"Constraint that is used to compute the cost for pressure in a zone. Since the cost of pressure typically appears in the objective function
-or is bounded, the constraints do not compute the price directly, rather they play a lower bound on the price of pressure, which is implictly tight
-when this term only appears in the objective funtion.
-``pc_z \\ge p_z[1] * \\pi_z^2 + cp_z[2] * \\pi_z + cp_z[3]
-where ``pc_z`` is the maximum pressure price in zone ``z`` and ``p_z`` is a quadractic function of the maximum pressure in ``z``.
-"
+"Constraint that is used to compute the cost for pressure in a zone. Since the cost of
+pressure typically appears in the objective function or is bounded, the constraints do
+not compute the price directly. Rather they act as a lower bound on the price of
+pressure, which is implictly tight when this term only appears in the objective function:
+
+```math
+pc_z \\ge p_z[1] * \\pi_z^2 + cp_z[2] * \\pi_z + cp_z[3],
+```
+
+where ``pc_z`` is the maximum pressure price in zone ``z`` and ``p_z`` is a quadratic
+function of the maximum pressure in ``z``."
 function constraint_pressure_price(gpm::AbstractGasPowerModel, i::Int; nw::Int=nw_id_default)
     if !haskey(_IM.con(gpm, _GM.gm_it_sym, nw), :pressure_price)
         _IM.con(gpm, _GM.gm_it_sym, nw)[:pressure_price] = Dict{Int, JuMP.ConstraintRef}()
@@ -138,9 +181,13 @@ function constraint_pressure_price(gpm::AbstractGasPowerModel, i::Int; nw::Int=n
     constraint_pressure_price(gpm, nw, i, price_zone["cost_p"])
 end
 
-"Constraint this used to compute the maximum pressure in a price zone. Since the maximum pressure typically appears in a minimization
-objective function, the max is modeled as a lower bound of the form
-``\\pi_z \\ge \\pi_i \\forall i \\in z`` "
+"Constraint that is used to compute the maximum pressure in a price zone. Since the
+maximum pressure typically appears in a minimization objective function, the max is
+modeled as a lower bound of the form
+
+```math
+\\pi_z \\ge \\pi_i \\forall i \\in z.
+```"
 function constraint_zone_pressure(gm::_GM.AbstractGasModel, i::Int; nw::Int=gm.cnw)
     junctions = filter(x -> x.second["price_zone"] == i, _GM.ref(gm, nw, :junction))
     constraint_zone_pressure(gm, nw, i, keys(junctions))
